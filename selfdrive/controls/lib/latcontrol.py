45ef704581871b3590f0a9162de1fc2c4cc9728c
Gernby <gernby2002@yahoo.com>
2018-12-28 13:15:50 -0600

    Reverted a couple resonant parameters
    Reduced projection_factor and increased accel_limit

--- selfdrive/controls/lib/latcontrol.py
+++ selfdrive/controls/lib/latcontrol.py
@@ -39,8 +39,8 @@ class LatControl(object):
     self.last_cloudlog_t = 0.0
     self.setup_mpc(CP.steerRateCost)
     self.smooth_factor = 2.0 * CP.steerActuatorDelay / _DT      # Multiplier for inductive component (feed forward)
-    self.projection_factor = CP.steerActuatorDelay            #  Mutiplier for reactive component (PI)
-    self.accel_limit = 2.0                                 # Desired acceleration limit to prevent "whip steer" (resistive component)
+    self.projection_factor = CP.steerActuatorDelay / 2.0      #  Mutiplier for reactive component (PI)
+    self.accel_limit = 5.0                                 # Desired acceleration limit to prevent "whip steer" (resistive component)
     self.ff_angle_factor = 0.5         # Kf multiplier for angle-based feed forward
     self.ff_rate_factor = 5.0         # Kf multiplier for rate-based feed forward
     self.prev_angle_rate = 0
